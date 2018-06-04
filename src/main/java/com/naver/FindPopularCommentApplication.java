94bf59a960917b1cabeba303c4142921cda3cc76
jaehee <akwogml112@gmail.com>
2018-06-04 13:30:59 +0900

    feat: 디렉토리 구성

---
+++ src/main/java/com/naver/FindPopularCommentApplication.java
@@ -0,0 +1,12 @@
+package com.naver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindPopularCommentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindPopularCommentApplication.class, args);
+	}
+}
