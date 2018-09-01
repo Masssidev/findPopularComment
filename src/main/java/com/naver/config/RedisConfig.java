be80d6cb4a894acc54171760e3aae74f5dfcc97d
jaehee <akwogml112@gmail.com>
2018-09-01 11:34:17 +0900

    feat: jedis 클라이언트 종속성 추가 redis 클라이언트 중 jedis를 사용하기 위하여 maven 종속성을 추가함

--- src/main/java/com/naver/config/RedisConfig.java
+++ src/main/java/com/naver/config/RedisConfig.java
@@ -1,39 +1,39 @@
-//package com.naver.config;
-//
-//import org.springframework.cache.annotation.EnableCaching;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-//import org.springframework.data.redis.core.RedisTemplate;
-//import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-//import org.springframework.data.redis.serializer.StringRedisSerializer;
-//import redis.clients.jedis.JedisPoolConfig;
-//
-//@Configuration
-//@EnableCaching
-//public class RedisConfig {
-//
-//	@Bean
-//	public JedisPoolConfig jedisPoolConfig() {
-//		JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
-//		return jedisPoolConfig;
-//	}
-//
-//	@Bean
-//	public JedisConnectionFactory jedisConnectionFactory(JedisPoolConfig jedisPoolConfig) {
-//		JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(jedisPoolConfig);
-//		return jedisConnectionFactory;
-//	}
-//
-//	@Bean(name = "redisTemplate")
-//	public RedisTemplate<String, Object> redisTemplateConfig(JedisConnectionFactory jedisConnectionFactory) {
-//		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
-//
-//		redisTemplate.setKeySerializer(new StringRedisSerializer());
-//		redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-//		redisTemplate.setConnectionFactory(jedisConnectionFactory);
-//
-//		return redisTemplate;
-//	}
-//	
-//}
+package com.naver.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import redis.clients.jedis.JedisPoolConfig;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+
+	@Bean
+	public JedisPoolConfig jedisPoolConfig() {
+		JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+		return jedisPoolConfig;
+	}
+
+	@Bean
+	public JedisConnectionFactory jedisConnectionFactory(JedisPoolConfig jedisPoolConfig) {
+		JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(jedisPoolConfig);
+		return jedisConnectionFactory;
+	}
+
+	@Bean(name = "redisTemplate")
+	public RedisTemplate<String, Object> redisTemplateConfig(JedisConnectionFactory jedisConnectionFactory) {
+		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
+
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+		redisTemplate.setConnectionFactory(jedisConnectionFactory);
+
+		return redisTemplate;
+	}
+	
+}
