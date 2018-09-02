b2e939fbd5e49a8023e5ce9b6cd8812094217b78
jaehee <akwogml112@gmail.com>
2018-09-02 23:57:45 +0900

    feat: 비동기 Config파일 주석 처리 필요할 때 사용할 것임 지금은 주석처리해 놓음

--- src/main/java/com/naver/config/AsyncConfig.java
+++ src/main/java/com/naver/config/AsyncConfig.java
@@ -1,22 +1,22 @@
-package com.naver.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.TaskExecutor;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-
-@Configuration
-@EnableAsync
-public class AsyncConfig {
-
-	@Bean
-	public TaskExecutor taskExecutor() {
-		ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
-		taskExecutor.setCorePoolSize(3);
-		taskExecutor.setQueueCapacity(20);
-		taskExecutor.setMaxPoolSize(5);
-		return taskExecutor;
-	}
-}
-
+//package com.naver.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.task.TaskExecutor;
+//import org.springframework.scheduling.annotation.EnableAsync;
+//import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+//
+//@Configuration
+//@EnableAsync
+//public class AsyncConfig {
+//
+//	@Bean
+//	public TaskExecutor taskExecutor() {
+//		ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+//		taskExecutor.setCorePoolSize(3);
+//		taskExecutor.setQueueCapacity(20);
+//		taskExecutor.setMaxPoolSize(5);
+//		return taskExecutor;
+//	}
+//}
+//
